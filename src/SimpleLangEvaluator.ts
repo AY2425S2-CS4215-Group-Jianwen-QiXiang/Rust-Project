e5349098c1d7f5650ff99676f836ecfa1038f812
Jianwen <jianwenlei2003@gmail.com>
2025-04-14 15:09:12 +0800

    Make adaptation to Source Academy front end

--- src/SimpleLangEvaluator.ts
+++ src/SimpleLangEvaluator.ts
@@ -68,13 +68,14 @@ type StringMatrixFunction = (arg: string[][]) => undefined;
 
 export class SimpleLangEvaluator extends BasicEvaluator {
     private executionCount: number;
-    private visitor : RustingCompiler;
+    private compiler : RustingCompiler;
     private typeChecker: SimpleLangTypeChecker;
+    private runner : RustingMachine
 
     constructor(conductor: IRunnerPlugin) {
         super(conductor);
         this.executionCount = 0;
-        this.visitor = new RustingCompiler();
+        this.compiler = new RustingCompiler();
         this.typeChecker = new SimpleLangTypeChecker()
     }
 
@@ -93,10 +94,11 @@ export class SimpleLangEvaluator extends BasicEvaluator {
             let global_ce : string[][] = []
             let global_type_ce : TypeClosure[][] = []
             this.typeChecker.visit(tree)(global_type_ce)
-            this.visitor.visit(tree)(global_ce)
+            this.compiler.visit(tree)(global_ce)
+            this.runner = new RustingMachine(this.compiler.instruction)
 
             // Send the result to the REPL
-            this.conductor.sendOutput(`Result of expression: ${this.visitor.instructions_for_display()}`);
+            this.conductor.sendOutput(`Result of expression: ${String(this.runner.run())}`);
         }  catch (error) {
             // Handle errors and send them to the REPL
             if (error instanceof Error) {
